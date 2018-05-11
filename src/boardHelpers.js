6f4d8661d9870408519213b58b697dd3dc0fada3
Jacob Adams <Artchecks@gmail.com>
2018-05-11 10:57:04 -0700

    refactored to run smoother, removed redundant code

--- src/boardHelpers.js
+++ src/boardHelpers.js
@@ -10,32 +10,31 @@ export function chainReveal (board, i, j, height, width, revealed) {
     adjustments.forEach(adjJ => {
       let adjustedI = i + adjI
       let adjustedJ = j + adjJ
+      if (!(adjustedI > -1 && adjustedI < height &&
+        adjustedJ > -1 && adjustedJ < width)) {
+        return
+      }
+      if (adjustedI === i && adjustedJ === j) {
+        return
+      }
+      if (isCoordsInReveal(revealList, adjustedI, adjustedJ)) {
+        return
+      }
       console.log(adjustedI, adjustedJ)
-      if (adjustedI > -1 && adjustedI < height &&
-        adjustedJ > -1 && adjustedJ < width &&
-        !(adjustedI === i && adjustedJ === j) &&
-        revealList.findIndex(coords => coords.i === adjustedI && coords.j === adjustedJ) === -1) {
-        // we're good in here!
-        revealList.push({i: adjustedI, j: adjustedJ})
-        if (board[adjustedI][adjustedJ].getContents() === null) {
-          revealList = arrayUnique([...revealList, ...chainReveal(board, adjustedI, adjustedJ, height, width, revealList)])
-        }
+      // we're good in here!
+      revealList.push({i: adjustedI, j: adjustedJ})
+      if (board[adjustedI][adjustedJ].getContents() === null) {
+        revealList = ([...revealList, ...chainReveal(board, adjustedI, adjustedJ, height, width, revealList)])
       }
     })
   })
   return revealList
 }
 
-function arrayUnique (array) {
-  var a = array.concat()
-  for (var i = 0; i < a.length; ++i) {
-    for (var j = i + 1; j < a.length; ++j) {
-      console.log('ding')
-      if (a[i] === a[j]) {
-        a.splice(j--, 1)
-      }
-    }
-  }
-
-  return a
+// checks if the i, j coords are in an object and returns a boolean
+function isCoordsInReveal (reveal, i, j) {
+  const revealed = reveal.find(function (coords) {
+    return coords.i === i && coords.j === j
+  })
+  return typeof revealed !== 'undefined'
 }
